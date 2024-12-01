44d5b1d630a19d9afa50bfd565643127954317da
Gabriel Hooks <gabriel.i.hooks@gmail.com>
2024-12-01 18:26:51 -0500

    vite config base

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,19 @@ import { ghPages } from 'vite-plugin-gh-pages';
 
 export default defineConfig({
   base: './',
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: './src/index.html',
+        imgmod: './src/imgmod.html',
+        ags: './src/ags.html',
+        timesheet: './src/timesheet.html',
+        research2: './src/research2.html',
+        research1: './src/research1.html',
+        coach: './src/coach.html'
+      }
+    }
+  }
 });
 
